07e12b4874f311597c171166af96f829ca8b5f41
NguyenLe1605 <84883483+NguyenLe1605@users.noreply.github.com>
2023-04-24 08:57:12 +0700

    Task 1

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,15 @@
+package synthesizer;
+
+public interface BoundedQueue<T> {
+    int capacity(); // return size of the buffer
+    int fillCount(); // return the number of items currently in the buffer
+    void enqueue(T x); // add item x to the end
+    T deque(); // delete and return element at the front
+    T peek(); // return (but do not element) from the front
+
+//     is the buffer empty
+    default boolean isEmpty() { return fillCount() == 0; }
+
+//    is the buffer full
+    default boolean isFull() { return fillCount() == capacity(); }
+}
