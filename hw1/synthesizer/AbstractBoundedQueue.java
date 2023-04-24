eafcfddc1cad210493ac56e4d6fe9086fba88631
NguyenLe1605 <84883483+NguyenLe1605@users.noreply.github.com>
2023-04-24 09:08:06 +0700

    Task 2

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,9 @@
+package synthesizer;
+
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    protected int capacity;
+    protected int fillCount;
+    public int capacity() { return capacity; }
+    public int fillCount() { return fillCount; }
+}
