aef38048a6f2880c8b47514be7c7d126ca6230ab
Duich1605 <dangnguyenhentai@gmail.com>
2023-05-31 10:49:03 +0700

    Add test case for OffByOne lowercase and uppercase

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -16,6 +16,8 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('b', 'a'));
         assertFalse(offByOne.equalChars('a', 'z'));
         assertTrue(offByOne.equalChars('%', '&'));
+        assertFalse(offByOne.equalChars('a', 'B'));
+        assertTrue(offByOne.equalChars('A', 'B'));
     }
 
 }
