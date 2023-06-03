f58c743e88ae69aadec3f2b5988c620f891fb26a
Duich1605 <dangnguyenhentai@gmail.com>
2023-06-03 07:33:55 +0700

    Fix backwash of percolation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -6,7 +6,7 @@ public class Percolation {
     private int[][] grid;
     private int numOfOpenSites;
     private WeightedQuickUnionUF wqu;
-    private int finalPos;
+    private WeightedQuickUnionUF topWqu;
     private int N;
     private static final int CLOSE_SITE = -1;
     private static final int OPEN_SITE = 0;
@@ -17,8 +17,11 @@ public class Percolation {
             {1, 0},
             {0, -1}
     };
+    private int topRow;
+    private int bottomRow;
     private static final int ROW_IDX = 0;
     private static final int COL_IDX = 1;
+
     public Percolation(int N) {
         if (N <= 0) {
             throw new IllegalArgumentException("Can not create percolation with N <= 0");
@@ -30,15 +33,13 @@ public class Percolation {
                 grid[i][j] = CLOSE_SITE;
             }
         }
-        wqu = new WeightedQuickUnionUF(N * N);
-        for (int i = 1; i < N; i++) {
-            wqu.union(0, i);
-        }
+//        Creating a wqu with 2 extra slots to represent the top of the grid, and the bottom of the grid
+        wqu = new WeightedQuickUnionUF(N * N + 2);
+        topWqu = new WeightedQuickUnionUF(N * N + 1);
 
-        finalPos = getArrayIndex(N - 1, N - 1);
-        for (int i = 0; i < N - 1; i++) {
-            wqu.union(finalPos, getArrayIndex(N - 1, i));
-        }
+        int finalPos = getArrayIndex(N - 1, N - 1);
+        this.topRow = finalPos + 1;
+        this.bottomRow = finalPos + 2;
         this.numOfOpenSites = 0;
     }
 
@@ -68,7 +69,7 @@ public class Percolation {
             return grid[row][col] == FULL_SITE;
         }
 
-        if (wqu.connected(0, getArrayIndex(row, col))) {
+        if (topWqu.connected(topRow, getArrayIndex(row, col))) {
             grid[row][col] = FULL_SITE;
             return true;
         }
@@ -86,18 +87,31 @@ public class Percolation {
         }
         numOfOpenSites++;
         grid[row][col] = row == 0 ? FULL_SITE : OPEN_SITE;
+        int idx = getArrayIndex(row, col);
+
+//        Connect the site with the surroundings, if the site is at the top row
+//        or the bottom row, connect them to the representation
         for (var direction : DIRECTIONS) {
             int newRow = row + direction[ROW_IDX];
             int newCol = col + direction[COL_IDX];
             if (checkIndex(newRow, newCol) &&
                     grid[newRow][newCol] != CLOSE_SITE
             ) {
-                wqu.union(getArrayIndex(row, col), getArrayIndex(newRow, newCol));
+                wqu.union(idx, getArrayIndex(newRow, newCol));
+                topWqu.union(idx, getArrayIndex(newRow, newCol));
             }
+        }
+        if (row == 0) {
+            wqu.union(topRow, idx);
+            topWqu.union(topRow, idx);
+        }
+
+        if (row == N - 1) {
+            wqu.union(bottomRow, getArrayIndex(row, col));
         }
     }
 
     public boolean percolates() {
-        return wqu.connected(0, finalPos);
+        return wqu.connected(topRow, bottomRow);
     }
 }
