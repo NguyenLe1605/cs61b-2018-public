13a0ac3cc38c95722c379c51c7f8834d36d87059
NguyenLe1605 <84883483+NguyenLe1605@users.noreply.github.com>
2023-06-03 09:05:51 +0700

    Add percolation stats implementation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -11,12 +11,7 @@ public class Percolation {
     private static final int CLOSE_SITE = -1;
     private static final int OPEN_SITE = 0;
     private static final int FULL_SITE = 1;
-    private static final int[][] DIRECTIONS = {
-            {-1, 0},
-            {0, 1},
-            {1, 0},
-            {0, -1}
-    };
+    private static final int[][] DIRECTIONS = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     private int topRow;
     private int bottomRow;
     private static final int ROW_IDX = 0;
@@ -33,7 +28,8 @@ public class Percolation {
                 grid[i][j] = CLOSE_SITE;
             }
         }
-//        Creating a wqu with 2 extra slots to represent the top of the grid, and the bottom of the grid
+        // Creating a wqu with 2 extra slots to represent the
+        // top of the grid, and the bottom of the grid
         wqu = new WeightedQuickUnionUF(N * N + 2);
         topWqu = new WeightedQuickUnionUF(N * N + 1);
 
@@ -92,14 +88,12 @@ public class Percolation {
         grid[row][col] = row == 0 ? FULL_SITE : OPEN_SITE;
         int idx = getArrayIndex(row, col);
 
-//        Connect the site with the surroundings, if the site is at the top row
-//        or the bottom row, connect them to the representation
+        // Connect the site with the surroundings, if the site is at the top row
+        // or the bottom row, connect them to the representation
         for (var direction : DIRECTIONS) {
             int newRow = row + direction[ROW_IDX];
             int newCol = col + direction[COL_IDX];
-            if (checkIndex(newRow, newCol) &&
-                    grid[newRow][newCol] != CLOSE_SITE
-            ) {
+            if (checkIndex(newRow, newCol) && grid[newRow][newCol] != CLOSE_SITE) {
                 wqu.union(idx, getArrayIndex(newRow, newCol));
                 topWqu.union(idx, getArrayIndex(newRow, newCol));
             }
@@ -116,5 +110,9 @@ public class Percolation {
 
     public boolean percolates() {
         return wqu.connected(topRow, bottomRow);
+    }
+
+    public static void main(String[] args) {
+
     }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,52 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private int T;
+    private double mean, stddev;
+    private double[] results;
+    private static final double CONFIDENCE_CONSTANT = 1.96;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.T = T;
+        results = new double[T];
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N and T must be <= 0");
+        }
+        for (int i = 0; i < N; i++) {
+            Percolation perc = pf.make(N);
+            while (!perc.percolates()) {
+                while (true) {
+                    int row = StdRandom.uniform(N);
+                    int col = StdRandom.uniform(N);
+                    if (!perc.isOpen(row, col)) {
+                        perc.open(row, col);
+                        break;
+                    }
+                }
+            }
+            results[i] = perc.numberOfOpenSites() * 1.0 / (N * N);
+        }
+
+        this.mean = StdStats.mean(results);
+        this.stddev = StdStats.stddev(results);
+    }
+
+    public double mean() {
+        return this.mean;
+    }
+
+    public double stddev() {
+        return this.stddev;
+    }
+
+    public double confidenceLow() {
+        return this.mean - (CONFIDENCE_CONSTANT * stddev) / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return this.mean + (CONFIDENCE_CONSTANT * stddev) / Math.sqrt(T);
+    }
 }
